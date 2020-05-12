6789c88f671341c3f70168cee85aa7be7f8e7d27
Norman <isaza00@hotmail.com>
2020-05-12 15:20:18 -0500

    until read_line function

--- main.c
+++ main.c
@@ -1,27 +1,36 @@
 #include "monty.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
+char *command = NULL;
+char *param = NULL;
+int count_lines = 0;
 
 int main(int argc, char *argv[])
 {
-	/*char *buffer_line;*/
-	char *monty_file;
+	FILE *monty_file;
+	int line = 1;
 
 	if (argc != 2)
 	{
 		dprintf(2, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	monty_file = open(argv[1], O_RDONLY);
+	monty_file = fopen(argv[1], "r");
 	if (monty_file == NULL)
 	{
 		dprintf(2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	void read_line(monty_file);
-	fclose(monty_file);
+	while (line == 1)
+	{
+		line = read_line(monty_file);
+		printf("command = %s and param = %s\n", command, param); 
+		monty_function();
+		count_lines++;
+	}
+	
+
+	
+	
+	pclose(monty_file);
 	return (0);
 }
